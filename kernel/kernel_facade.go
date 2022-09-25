ffab804fd2dc5f224d413ba510afff50c821d93d
むずでょ <muzudho1@gmail.com>
2022-09-25 22:16:26 +0900

    LoadRenDb

--- kernel/kernel_facade.go
+++ kernel/kernel_facade.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -26,7 +25,10 @@ func (k *Kernel) LoadRenDb(path string, onError func(error) bool) bool {
 
 	// 外部ファイルからの入力を、内部状態へ適用
 	for _, ren := range db.Rens {
-		k.RefreshRenToInternal(ren)
+		var isOk = k.RefreshRenToInternal(ren)
+		if !isOk {
+			return false
+		}
 	}
 
 	// 差し替え
@@ -35,7 +37,7 @@ func (k *Kernel) LoadRenDb(path string, onError func(error) bool) bool {
 }
 
 // RefreshRenToInternal - TODO 外部ファイルから入力された内容を内部状態に適用します
-func (k *Kernel) RefreshRenToInternal(r *Ren) {
+func (k *Kernel) RefreshRenToInternal(r *Ren) bool {
 	{
 		var getDefaultStone = func() (bool, Stone) {
 			panic(fmt.Sprintf("unexpected stone:%s", r.Sto))
@@ -44,33 +46,43 @@ func (k *Kernel) RefreshRenToInternal(r *Ren) {
 		// TODO stone from r.Sto
 		// Example: "x" --> black
 		var isOk, stone = GetStoneFromChar(r.Sto, getDefaultStone)
-		if isOk {
-			r.stone = stone
+		if !isOk {
+			return false
 		}
+		r.stone = stone
 	}
 	{
 		// TODO locations from r.Loc
 		// Example: "C1 D1 E1"
-		var codes = strings.Split(r.Loc, " ")
+		if 0 < len(r.Loc) {
+			var codes = strings.Split(r.Loc, " ")
 
-		var numbers = []Point{}
-		for _, code := range codes {
-			// TODO board
-			var number, err = strconv.Atoi(code)
-			if err != nil {
-				panic(err)
+			var numbers = []Point{}
+			for _, code := range codes {
+				var location = k.Board.GetPointFromCode(code)
+				numbers = append(numbers, location)
 			}
 
-			numbers = append(numbers, Point(number))
+			r.locations = numbers
 		}
-
-		r.locations = numbers
 	}
 	{
 		// TODO libertyLocations from r.LibLoc
 		// Example: "F1 E2 D2 B1 C2"
+		if 0 < len(r.LibLoc) {
+			var codes = strings.Split(r.LibLoc, " ")
 
+			var numbers = []Point{}
+			for _, code := range codes {
+				var location = k.Board.GetPointFromCode(code)
+				numbers = append(numbers, location)
+			}
+
+			r.libertyLocations = numbers
+		}
 	}
+
+	return true
 }
 
 // RemoveRen - 石の連を打ち上げます
