19d87b1afe28a688b77bd4f459e98508663baec3
むずでょ <muzudho1@gmail.com>
2022-10-13 19:35:49 +0900

    BC

--- kernel/o12o__10o2o_1o1o0_board_coordinate.go
+++ kernel/o12o__10o2o_1o1o0_board_coordinate.go
@@ -7,6 +7,12 @@ import (
 	"strconv"
 )
 
+// 片方の枠の厚み。東、北、西、南
+const oneSideWallThickness = 1
+
+// 両側の枠の厚み。南北、または東西
+const bothSidesWallThickness = 2
+
 // BoardCoordinate - 盤座標
 type BoardCoordinate struct {
 	// 枠付きの盤の水平一辺の交点の要素数
@@ -28,6 +34,21 @@ func (bc *BoardCoordinate) GetMemoryBoardHeight() int {
 // GetMemoryBoardArea - 壁付き盤の面積
 func (bc *BoardCoordinate) GetMemoryBoardArea() int {
 	return bc.GetMemoryBoardWidth() * bc.GetMemoryBoardHeight()
+}
+
+func (bc *BoardCoordinate) GetBoardWidth() int {
+	// 枠の分、２つ減らす
+	return bc.memoryWidth - bothSidesWallThickness
+}
+
+func (bc *BoardCoordinate) GetBoardHeight() int {
+	// 枠の分、２つ減らす
+	return bc.memoryHeight - bothSidesWallThickness
+}
+
+// GetBoardArea - 壁無し盤の面積
+func (bc *BoardCoordinate) GetBoardArea() int {
+	return bc.GetBoardWidth() * bc.GetBoardWidth()
 }
 
 // GetXFromFile - `A` ～ `Z` を 0 ～ 24 へ変換します。 国際囲碁連盟のルールに倣い、筋の符号に `I` は使いません
@@ -100,6 +121,33 @@ func GetRankFromCode(code string) string {
 	}
 
 	return code[1:2]
+}
+
+// GetPointFromXy - x,y 形式の座標を、 point （配列のインデックス）へ変換します。
+// point は壁を含む盤上での座標です
+//
+// Parameters
+// ----------
+// x : int
+//
+//	壁を含まない盤での筋番号。 Example: 19路盤なら0～18
+//
+// y : int
+//
+//	壁を含まない盤での段番号。 Example: 19路盤なら0～18
+//
+// Returns
+// -------
+// point : Point
+//
+//	配列インデックス。 Example: 2,3 なら 65
+func (bc *BoardCoordinate) GetPointFromXy(x int, y int) Point {
+	return Point(y*bc.memoryWidth + x)
+}
+
+// GetXyFromPoint - `GetPointFromXy` の逆関数
+func (bc *BoardCoordinate) GetXyFromPoint(point Point) (int, int) {
+	return getXyFromPointOnBoard(bc.memoryWidth, point)
 }
 
 // EOF [O12o__10o2o_1o1o0]
--- kernel/o12o__11o1o0_board.go
+++ kernel/o12o__11o1o0_board.go
@@ -2,23 +2,12 @@
 
 package kernel
 
-// 片方の枠の厚み。東、北、西、南
-const oneSideWallThickness = 1
-
-// 両側の枠の厚み。南北、または東西
-const bothSidesWallThickness = 2
-
 // Board - 盤
 type Board struct {
 	// ゲームルール
 	gameRule GameRule
 	// 盤座標
 	coordinate BoardCoordinate
-
-	// 枠付きの横幅
-	memoryWidth int
-	// 枠付きの縦幅
-	memoryHeight int
 
 	// 交点
 	//
@@ -63,26 +52,6 @@ func (b *Board) GetCoordinate() *BoardCoordinate {
 	return &b.coordinate
 }
 
-// GetMemoryWidth - 枠の厚みを含んだ横幅
-func (b *Board) GetMemoryWidth() int {
-	return b.memoryWidth
-}
-
-// GetMemoryHeight - 枠の厚みを含んだ縦幅
-func (b *Board) GetMemoryHeight() int {
-	return b.memoryHeight
-}
-
-// GetWidth - 枠の厚みを含まない横幅
-func (b *Board) GetWidth() int {
-	return b.memoryWidth - bothSidesWallThickness
-}
-
-// GetHeight - 枠の厚みを含まない縦幅
-func (b *Board) GetHeight() int {
-	return b.memoryHeight - bothSidesWallThickness
-}
-
 // GetStoneAt - 指定座標の石を取得
 func (b *Board) GetStoneAt(i Point) Stone {
 	return b.cells[i]
@@ -98,28 +67,6 @@ func (b *Board) GetColorAt(i Point) Color {
 	return b.cells[i].GetColor()
 }
 
-// GetPointFromXy - 座標変換 (x,y) → Point
-//
-// Parameters
-// ----------
-// x : int
-//	筋番号。 Example: 19路盤なら0～18
-// y : int
-//	段番号。 Example: 19路盤なら0～18
-//
-// Returns
-// -------
-// point : Point
-//  配列インデックス。 Example: 2,3 なら 65
-func (b *Board) GetPointFromXy(x int, y int) Point {
-	return Point(y*b.memoryWidth + x)
-}
-
-// GetXyFromPoint - `GetPointFromXy` の逆関数
-func (b *Board) GetXyFromPoint(point Point) (int, int) {
-	return getXyFromPointOnBoard(b.memoryWidth, point)
-}
-
 func getXyFromPointOnBoard(memoryWidth int, point Point) (int, int) {
 	var p = int(point)
 	return p % memoryWidth, p / memoryWidth
@@ -127,17 +74,12 @@ func getXyFromPointOnBoard(memoryWidth int, point Point) (int, int) {
 
 // サイズ変更
 func (b *Board) resize(width int, height int) {
-	b.memoryWidth = width + bothSidesWallThickness
-	b.memoryHeight = height + bothSidesWallThickness
-	b.cells = make([]Stone, b.getMemoryArea())
+	b.coordinate.memoryWidth = width + bothSidesWallThickness
+	b.coordinate.memoryHeight = height + bothSidesWallThickness
+	b.cells = make([]Stone, b.coordinate.GetMemoryBoardArea())
 
 	// ４方向（東、北、西、南）の番地への相対インデックス
-	b.Direction = [4]int{1, -b.GetMemoryWidth(), -1, b.GetMemoryWidth()}
-}
-
-// 枠付き盤の面積
-func (b *Board) getMemoryArea() int {
-	return b.memoryWidth * b.memoryHeight
+	b.Direction = [4]int{1, -b.coordinate.GetMemoryBoardWidth(), -1, b.coordinate.GetMemoryBoardWidth()}
 }
 
 // EOF [O12o__11o1o0]
