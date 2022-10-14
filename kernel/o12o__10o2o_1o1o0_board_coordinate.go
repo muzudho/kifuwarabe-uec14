b7aeeb3b2d7c53f348d688027de83672d4ec57c3
むずでょ <muzudho1@gmail.com>
2022-10-14 19:59:18 +0900

    BC

--- kernel/o12o__10o2o_1o1o0_board_coordinate.go
+++ kernel/o12o__10o2o_1o1o0_board_coordinate.go
@@ -230,6 +230,25 @@ func (bc *BoardCoordinate) GetPointFromGtpMove(gtp_move string) Point {
 		GetYFromRank(GetRankFromCode(gtp_move))+oneSideWallThickness)
 }
 
+func getFileRankFromPointOnBoard(memoryWidth int, point Point) (string, int) {
+	var x, y = getXyFromPointOnBoard(memoryWidth, point)
+	var file = GetFileFromX(x - oneSideWallThickness)
+	var rank = getRankFromY(y) - oneSideWallThickness
+	return file, rank
+}
+
+// 例えば "A1" のように、行番号をゼロサプレスして返す
+func getCodeFromPointOnBoard(memoryWidth int, point Point) string {
+	var file, rank = getFileRankFromPointOnBoard(memoryWidth, point)
+	return fmt.Sprintf("%s%d", file, rank)
+}
+
+// 例えば "01A" のように、符号を行、列の順とし、かつ、行番号を一律２桁のゼロ埋めにします
+func getRenIdFromPointOnBoard(memoryWidth int, point Point) string {
+	var file, rank = getFileRankFromPointOnBoard(memoryWidth, point)
+	return fmt.Sprintf("%02d%s", rank, file)
+}
+
 // GetGtpMoveFromPoint - `GetPointFromGtpMove` の逆関数
 func (bc *BoardCoordinate) GetGtpMoveFromPoint(point Point) string {
 	return getCodeFromPointOnBoard(bc.memoryWidth, point)
