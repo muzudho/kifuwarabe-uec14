bb1ac589d6f5fbea4483e9f1889cf486f33dd778
むずでょ <muzudho1@gmail.com>
2022-10-15 16:08:21 +0900

    Update o22o1o1o0_masonry.go

--- kernel/o22o1o1o0_masonry.go
+++ kernel/o22o1o1o0_masonry.go
@@ -2,19 +2,10 @@
 
 package kernel
 
-import "fmt"
-
-func (k *Kernel) IsMasonryError(stone Stone, point Point) bool {
-	var target = k.Board.cells[point]
-
-	switch target {
-	case Stone_Space:
-		return false
-	case Stone_Black, Stone_White, Stone_Wall:
-		return true
-	default:
-		panic(fmt.Sprintf("unexpected target cell:%s", target))
-	}
+// IsMasonry - 石の上に石を置こうとしたか？
+func (b *Board) IsMasonry(point Point) bool {
+	// 空点以外に石を置こうとしたら、石の上に石を置いた扱いにする
+	return !b.IsSpaceAt(point)
 }
 
 // EOF [O22o1o1o0]
