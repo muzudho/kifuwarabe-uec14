195482c917a3e3a5ab553b2795d06f943f9d78cd
むずでょ <muzudho1@gmail.com>
2022-10-15 14:10:14 +0900

    Check board

--- docs/qiita/o22o2o0_get_liberty.md
+++ docs/qiita/o22o2o0_get_liberty.md
@@ -71,13 +71,13 @@ type CheckBoard struct {
 	// 盤座標
 	coordinate BoardCoordinate
 
-	// 交点
+	// 長さが可変な盤
 	//
 	// * 英語で交点は node かも知れないが、表計算でよく使われる cell の方を使う
 	cells []Mark
 }
 
-// NewDirtyCheckBoard - 新規作成しますが、初期化されていない
+// NewDirtyCheckBoard - 新規作成するが、初期化されていない
 //
 // * このメソッドを呼び出した後に Init 関数を呼び出してほしい
 func NewDirtyCheckBoard() *CheckBoard {
@@ -101,6 +101,26 @@ func (cb *CheckBoard) Init(newBoardCoordinate BoardCoordinate) {
 	for p := Point(0); p < Point(len(cb.cells)); p++ {
 		cb.cells[p] = Mark_BitAllZeros
 	}
+}
+
+// GetAllBitsAt - 指定した交点の目印を取得
+func (cb *CheckBoard) GetAllBitsAt(point Point) Mark {
+	return cb.cells[point]
+}
+
+// SetAllBitsAt - 指定した交点に目印を設定
+func (cb *CheckBoard) SetAllBitsAt(point Point, mark Mark) {
+	cb.cells[point] = mark
+}
+
+// ClearAllBitsAt - フラグを消す
+func (cb *CheckBoard) ClearAllBitsAt(point Point) {
+	cb.cells[point] = Mark(0)
+}
+
+// IsZeroAt - 指定した交点に目印は付いていないか？
+func (cb *CheckBoard) IsZeroAt(point Point) bool {
+	return cb.cells[point] == Mark_BitAllZeros
 }
 
 // Overwrite - 上書き
--- kernel/o22o2o2o0_check_board.go
+++ kernel/o22o2o2o0_check_board.go
@@ -22,7 +22,7 @@ type CheckBoard struct {
 	cells []Mark
 }
 
-// NewDirtyCheckBoard - 新規作成しますが、初期化されていない
+// NewDirtyCheckBoard - 新規作成するが、初期化されていない
 //
 // * このメソッドを呼び出した後に Init 関数を呼び出してほしい
 func NewDirtyCheckBoard() *CheckBoard {
@@ -33,7 +33,7 @@ func NewDirtyCheckBoard() *CheckBoard {
 	return cb
 }
 
-// Init - 盤面初期化
+// Init - 初期化
 func (cb *CheckBoard) Init(newBoardCoordinate BoardCoordinate) {
 	// 盤面のサイズが異なるなら、盤面を作り直す
 	if cb.coordinate.memoryWidth != newBoardCoordinate.memoryWidth || cb.coordinate.memoryHeight != newBoardCoordinate.memoryHeight {
@@ -46,6 +46,26 @@ func (cb *CheckBoard) Init(newBoardCoordinate BoardCoordinate) {
 	for p := Point(0); p < Point(len(cb.cells)); p++ {
 		cb.cells[p] = Mark_BitAllZeros
 	}
+}
+
+// GetAllBitsAt - 指定した交点の目印を取得
+func (cb *CheckBoard) GetAllBitsAt(point Point) Mark {
+	return cb.cells[point]
+}
+
+// SetAllBitsAt - 指定した交点に目印を設定
+func (cb *CheckBoard) SetAllBitsAt(point Point, mark Mark) {
+	cb.cells[point] = mark
+}
+
+// ClearAllBitsAt - フラグを消す
+func (cb *CheckBoard) ClearAllBitsAt(point Point) {
+	cb.cells[point] = Mark(0)
+}
+
+// IsZeroAt - 指定した交点に目印は付いていないか？
+func (cb *CheckBoard) IsZeroAt(point Point) bool {
+	return cb.cells[point] == Mark_BitAllZeros
 }
 
 // Overwrite - 上書き
